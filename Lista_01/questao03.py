e121ea3f737884342a85558008d1378836d6f346
Paula Regina <pregiina@gmail.com>
2022-03-17 13:29:27 -0400

    Questao 03

---
+++ Lista_01/questao03.py
@@ -0,0 +1,5 @@
+'''Faça um Programa que peça dois números e imprima a soma.'''
+numero_1 = float(input("Informe o primeiro número: "))
+numero_2 = float(input("Informe o segundo número: "))
+soma = numero_1 + numero_2
+print(f'Resultado da Soma: {soma}')
