7f2c61d74b7d760e851f026cdbd85af9ae55d7c1
Paula Regina <pregiina@gmail.com>
2022-03-17 13:38:07 -0400

    Questao 05

---
+++ Lista_01/questao05.py
@@ -0,0 +1,4 @@
+'''Faça um Programa que converta metros para centímetros.'''
+medida_metros = float(input("Informe a medida em METROS: "))
+medida_cm = medida_metros*100
+print(f'Tamanho equivalente a: {medida_cm} cm')
