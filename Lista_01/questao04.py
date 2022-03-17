46db29aaf43004090844b3871d6d0e18a622c772
Paula Regina <pregiina@gmail.com>
2022-03-17 13:34:26 -0400

    Questao 04

---
+++ Lista_01/questao04.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
+nota_1 = float(input("Informe a nota 01: "))
+nota_2 = float(input("Informe a nota 02: "))
+nota_3 = float(input("Informe a nota 03: "))
+nota_4 = float(input("Informe a nota 04: "))
+media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+print(f'Media: {media}')
